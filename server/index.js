ecfaf04795285d35edc4b0aae3674f006c4f62de
ablondel19 <ablondel@student.s19.be>
2023-08-12 00:04:01 +0200

    Downgraded a few packages to enable server-side pagination / Transactions page done / starting geographic customers data chart

--- server/index.js
+++ server/index.js
@@ -11,10 +11,11 @@ import managementRoutes from './routes/management.js';
 import salesRoutes from './routes/sales.js';
 
 /* DUMMY DATA */
-// import { dataUser, dataProduct, dataProductStat } from './data/index.js';
+// import { dataUser, dataProduct, dataProductStat, dataTransaction } from './data/index.js';
 // import User from './models/User.js';
 // import Product from './models/Product.js';
 // import ProductStat from './models/ProductStat.js';
+// import Transaction from './models/Transaction.js';
 
 /* CONFIGURATION */
 dotenv.config();
@@ -47,6 +48,7 @@ mongoose
     // User.insertMany(dataUser);
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
+    // Transaction.insertMany(dataTransaction);
   })
   .catch((error) => {
     console.log(error);
